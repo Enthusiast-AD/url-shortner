a7d765ec2b8abe860c5171c5d16f9f646f00cf63
Ansh Deep <ad0811@srmist.edu.in>
2026-03-08 14:28:57 +0530

    initialize backend with Express, CORS, and cookie parser; set up URL and index routes

--- backend/src/models/url.js
+++ backend/src/models/url.js
@@ -1,16 +1,32 @@
 import mongoose, {Schema} from "mongoose";
 
 const urlSchema = new Schema(
-	{
-	    _id: string,
-	    originalUrl: {
-		type: String,
-		required: true,
-		lowercase: true.
-		trim: true,
-	    },
-	},
-	{timestamps: true}
+    {
+        shortId: {
+            type: String,
+            required: true,
+            unique: true,
+            index: true, 
+            trim: true
+        },
+        originalUrl: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        clicks: {
+            type: Number,
+            default: 0,
+        },
+        analytics: [
+            {
+                timestamp: { type: Date, default: Date.now },
+                ip: String,
+                userAgent: String
+            }
+        ]
+    },
+    {timestamps: true}
 )
 
 export const Url = mongoose.model("Url", urlSchema);
